dd837870ab1a805ee7dafb79f90bb929e5beb1c4
fedemolina <federicoandresmolina@gmail.com>
2021-11-23 01:00:35 -0300

    debugeando

--- R/PDAopt_MOD.R
+++ R/PDAopt_MOD.R
@@ -27,38 +27,65 @@
 #' PDA.proj.result$projbest
 #' 
 
-PDAopt_MOD <- function(origclass,origdata,q=1,weight=TRUE,lambda=0.1,...){ 
-  data.std <- as.matrix(origdata)
-  class.table <- table(origclass)
-  g<-length(class.table)
-  class.name<-names(class.table)
-  p<-ncol(data.std)
-  n<-nrow(data.std)
-  mean.g<-matrix(apply(data.std,2,function(x) 
-    tapply(x,origclass,mean,na.rm=TRUE)),ncol=p)
-  mean.all<-matrix(apply(data.std,2,mean),ncol=p)           
-  B<-matrix(0,ncol=p,nrow=p)
-  W<-matrix(0,ncol=p,nrow=p) 
-  for(i in 1:length(class.table)){
-    sel.id<-which(origclass==class.name[i])
-    temp.m1<-mean.g[i,]-mean.all
-    temp.m2<-data.std[sel.id,]-
-      matrix(1,length(sel.id),ncol=1)%*%mean.g[i,,drop=FALSE]
-    gn1<-ifelse(weight,length(sel.id),n/g)
-    B<-B+gn1*t(temp.m1)%*%temp.m1
-    W<-W+gn1*t(temp.m2)%*%temp.m2/length(sel.id)      
+PDAopt_MOD <-
+  function(origclass,
+           origdata,
+           q = 1,
+           weight = TRUE,
+           lambda = 0.1,
+           ...) {
+    # Comentarios para debugear
+    cat("\n\n ---- Corrida PDAopt_MOD ---- \n\n")
+    cat("dimensión de origdata: ", dim(origdata), "\n")
+    data.std <- as.matrix(origdata)
+    class.table <- table(origclass)
+    cat("class.table: ", class.table, "\n")
+    g <- length(class.table)
+    cat("g = ", g, "\n")
+    class.name <- names(class.table)
+    cat("class.name = ", class.name, "\n")
+    p <- ncol(data.std)
+    n <- nrow(data.std)
+    # comentarios
+    cat("p = ", p, "\n")
+    cat("n = ", n, "\n")
+    cat("dimensión data.std: ", dim(data.std), "\n")
+    cat("origclass = ", origclass, "\n")
+    mean.g <- matrix(apply(data.std, 2, function(x) {
+      tapply(x, origclass, mean, na.rm = TRUE)
+    }),
+    ncol = p)
+    cat("dimensión mean.g: ", dim(mean.g), "\n")
+    mean.all <- matrix(apply(data.std, 2, mean), ncol = p)
+    B <- matrix(0, ncol = p, nrow = p)
+    W <- matrix(0, ncol = p, nrow = p)
+    for (i in 1:length(class.table)) {
+      sel.id <- which(origclass == class.name[i])
+      temp.m1 <- mean.g[i, ] - mean.all
+      temp.m2 <- data.std[sel.id, ] -
+        matrix(1, length(sel.id), ncol = 1) %*% mean.g[i, , drop = FALSE]
+      gn1 <- ifelse(weight, length(sel.id), n / g)
+      B <- B + gn1 * t(temp.m1) %*% temp.m1
+      W <- W + gn1 * t(temp.m2) %*% temp.m2 / length(sel.id)
+    }
+    
+    W.t <- (1 - lambda) * W
+    diag(W.t) <- diag(W)
+    WB.t <- W.t + B
+    opt <- eigen(MASS::ginv(WB.t) %*% B)
+    
+    optVector <- matrix(as.numeric(opt$vectors[, 1:q]), ncol = q)
+    proj.data <- data.std %*% optVector
+    optindex <-
+      PDAindex2(origclass, proj.data, optVector, weight, lambda = lambda)
+    optobj <-
+      list(
+        indexbest = optindex,
+        projbest = optVector,
+        origclass = origclass,
+        origdata = origdata,
+        data.std = data.std
+      )
+    class(optobj) <- append(class(optobj), "PPoptim")
+    return(optobj)
   }
-  
-  W.t<-(1-lambda)*W
-  diag(W.t)<-diag(W)
-  WB.t<-W.t+B
-  opt<-eigen(MASS::ginv(WB.t)%*%B)
-  
-  optVector<-matrix(as.numeric(opt$vectors[,1:q]),ncol=q)
-  proj.data<-data.std%*%optVector
-  optindex<-PDAindex2(origclass,proj.data,optVector,weight,lambda=lambda)  
-  optobj<-list(indexbest=optindex,projbest=optVector,origclass=origclass,
-               origdata=origdata,data.std=data.std)
-  class(optobj)<-append(class(optobj),"PPoptim")
-  return(optobj)
-}
