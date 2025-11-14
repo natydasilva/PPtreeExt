a07cf3f5f60710cb8d36c00d766743f1daa86fba
natydasilva <natalia.dasilva@fcea.edu.uy>
2025-11-15 00:14:34 +1100

    clean unused arguments and fixed stoping rule with three conditions

--- R/TreeExt.construct.R
+++ R/TreeExt.construct.R
@@ -4,8 +4,8 @@
 #' classification in each split.
 #' @title Projection pursuit classification tree MOD
 #' @usage TreeExt.construct(origclass, origdata, Tree.Struct, id, rep, rep1, rep2,
-#' projbest.node, splitCutoff.node, PPmethod, r = NULL, 
-#' lambda = NULL, TOL, maxiter = 50000, q = 1, weight = TRUE, tol = .5, strule, tot,...) 
+#' projbest.node, splitCutoff.node, PPmethod, 
+#' lambda = NULL, q = 1, weight = TRUE, srule=TRUE, tot=NULL, tol = .5,...) 
 #' @param origclass original class 
 #' @param origdata original data
 #' @param Tree.Struct tree structure of projection pursuit classification tree
@@ -16,125 +16,138 @@
 #' @param projbest.node bests projection node
 #' @param splitCutoff.node cutof node
 #' @param PPmethod method for projection pursuit; "LDA", "PDA"
-#' @param r r in Lr index
 #' @param lambda lambda in PDA index
-#' @param TOL tolerance value for optimization algorithm
-#' @param maxiter max number of iterations for optimization algorithm
 #' @param q numeric value with dimension of the projected data, if it is 1 then 1D projection is used
 #' @param weight weight flag in LDA, PDA 
+#' @param srule stopping rule flag; if TRUE use stopping rule, if FALSE stop only for pure or empty nodes
+#' @param tot total number of observations
 #' @param tol tolerance value for entropy stopping rule for splitting a node
-#' @param strule select the stoping rule based in G=1 pure node
-#' @param tot total obs original class
 #' @param ... additional arguments to pass trough
 #' @useDynLib PPtreeExt
 #' @importFrom Rcpp evalCpp
 #' @export
-TreeExt.construct <- function(origclass,origdata,Tree.Struct, id, rep, rep1, rep2, projbest.node, splitCutoff.node, PPmethod,
-           r = NULL, lambda = NULL, TOL, maxiter = 50000, q = 1, weight = TRUE, tol = .5, strule, tot,...) {
-    #mod fede
-    # flag = FALSE
-    # if(!is.matrix(origdata)) {
-    #   flag <- TRUE
-    # }
-    origclass <- as.integer(origclass)
-    origdata <- as.matrix(origdata)
-    n <- nrow(origdata)
-    g <- table(origclass)
-    G <- length(g)
-    if(length(Tree.Struct) == 0){
-      #Tree.Struct<-matrix(1:(2*G-1),ncol=1)
-      # Tree.Struct<- cbind(Tree.Struct,0,0,0,0)
-      Tree.Struct <- matrix(0, nrow = 1, ncol = 5)
-    }
-    if (id > nrow(Tree.Struct) ) {
-      Tree.Struct <- rbind(Tree.Struct, matrix(0, nrow=id - nrow(Tree.Struct), ncol=5) )
-    }
-    ##To see the effect of diferent rules
-    #end.node = (G==1 | length(origclass)/tot <= .5| entropy(origclass)<tol)
-    end.node <- 0
-    if(strule == 1) {
-      end.node <- 1*(G == 1)
-    }else if(strule == 2) {
-      end.node <- 1*(length(origclass)/tot <= .05)
-    }else{
-      end.node <- 1*(entropy(origclass) < tol)
-    }
-    #end.node = (G==1 | length(origclass) <= 30 | entropy(origclass)<tol)
-    #,pure=TRUE,nodesize=FALSE,entronode=FALSE,tot,
-    cnd <- (end.node == 1) | (nrow(origdata) < 10)
-    #Mod fede
-    #cnd <- (end.node == 1) | (NROW(origdata) < 10) | !is.matrix(origdata) | flag
-    
-    
-    if( cnd  ){
-      #if (nrow(origdata) > 0 ) {
-      Tree.Struct[id,3] <- as.integer( names(g)[which.max(g)] )
-      Tree.Struct[,1] <- 1:nrow(Tree.Struct)
-      list(Tree.Struct=Tree.Struct,projbest.node=projbest.node, 
-           splitCutoff.node=splitCutoff.node,rep=rep,rep1=rep1,rep2=rep2)
-      # } else {
-      #   list(Tree.Struct=Tree.Struct,projbest.node=projbest.node, 
-      #        splitCutoff.node=splitCutoff.node,rep=rep,rep1=rep1,rep2=rep2)
-      # }
-    } else {
-      
-      Tree.Struct.row <- numeric(5)
-      Tree.Struct.row[1] <- id
-      Tree.Struct.row[2]<-rep1
-      rep1<-rep1+1
-      Tree.Struct.row[3]<-rep1
-      rep1<-rep1+1
-      Tree.Struct.row[4]<-rep2
-      rep2<-rep2+1
-      a<-findproj_Ext(origclass,origdata,PPmethod,q=1,weight=TRUE,lambda)
-      #a<-findproj_modLDA(origclass,origdata )
-      Tree.Struct.row[5]<-a$Index
-      
-      
-      Tree.Struct[id, ] <- Tree.Struct.row
-      
-      splitCutoff.node<-rbind(splitCutoff.node, a$C)
-      projbest.node<-rbind(projbest.node,matrix(a$Alpha, ncol=length(a$Alpha)) )
-      t.class<-origclass
-      t.data<-origdata
-      t.class<-t.class*a$IOindexL
-      t.n<-length(t.class[t.class==0])
-      t.index<-sort.list(t.class)
-      t.index<-sort(t.index[-(1:t.n)])
-      t.class<-t.class[t.index]
-      t.data<-origdata[t.index,]
-      
-      #Tree.Struct<- Tree.Struct
-      
-      b<-TreeExt.construct(t.class,t.data,Tree.Struct, 
-                            Tree.Struct[id, 2],rep,rep1,rep2,projbest.node, 
-                            splitCutoff.node,PPmethod,r,lambda,TOL,maxiter,strule=strule,tot=tot,...)
-      Tree.Struct<-b$Tree.Struct
-      projbest.node<-b$projbest.node
-      splitCutoff.node<-b$splitCutoff.node
-      rep<-b$rep
-      rep1<-b$rep1
-      rep2<-b$rep2
-      t.class<-origclass
-      t.data<-origdata
-      t.class<-(t.class*a$IOindexR)
-      t.n<-length(t.class[t.class==0])
-      t.index<-sort.list(t.class)
-      t.index<-sort(t.index[-(1:t.n)])
-      t.class<-t.class[t.index]
-      t.data<-origdata[t.index,]
-      n<-nrow(t.data)
-      G<-length(table(t.class))
-      b<-TreeExt.construct(t.class,t.data,Tree.Struct, 
-                            Tree.Struct[id,3],rep,rep1,rep2,projbest.node, 
-                            splitCutoff.node,PPmethod,r,lambda,TOL,maxiter,strule=strule,tot=tot,...)
-      Tree.Struct<-b$Tree.Struct
-      projbest.node<-b$projbest.node
-      splitCutoff.node<-b$splitCutoff.node
-      rep<-b$rep
-      rep1<-b$rep1
-      rep2<-b$rep2
-    }
-    list(Tree.Struct=Tree.Struct,projbest.node=projbest.node, 
-         splitCutoff.node=splitCutoff.node,rep=rep,rep1=rep1,rep2=rep2)
+TreeExt.construct <- function(origclass, origdata, Tree.Struct, id, rep, rep1,
+                              rep2, projbest.node, splitCutoff.node,
+                              PPmethod, lambda = NULL,
+                               q = 1, weight = TRUE,
+                              srule=TRUE, tot = NULL, tol = .5,...) {
+  
+  origclass <- as.integer(origclass)
+  origdata <- as.matrix(origdata)
+  n <- nrow(origdata)
+  g <- table(origclass)
+  G <- length(g)
+  if (length(Tree.Struct) == 0) {
+    Tree.Struct <- matrix(0, nrow = 1, ncol = 5)
   }
+  if (id > nrow(Tree.Struct)) {
+    Tree.Struct <- rbind(Tree.Struct, matrix(0, nrow = id - nrow(Tree.Struct), ncol =
+                                               5))
+  }
+  
+  
+  # if (length(origclass) == 0 || length(g) == 0) {
+  #   cnd <- TRUE
+  # } else {
+  #   if (srule) {
+  #     ent <- entropy(origclass)
+  #     if (length(ent) == 0 || is.na(ent)) ent <- 0
+  #     
+  #     cnd <- (G == 1) |
+  #       (length(origclass)/tot <= 0.05) |
+  #       (ent < tol)
+  #   } else {
+  #     cnd <- (G == 1)
+  #   }
+  # }
+  
+  if(srule){
+   if( is.na(entropy(origclass))) ent <- 0 
+   if (length(entropy(origclass)) == 0 ) ent <- 0
+    # ent <- entropy(origclass)
+  #if (length(ent) == 0 || is.na(entropy(origclass)) ent <- 0
+  cnd <- (G == 1) |
+          (length(origclass)/tot <= 0.05) |
+         (entropy(origclass) < tol)
+  }else{
+  cnd <- (G==1)
+  }
+  
+  if (cnd) {
+    Tree.Struct[id, 3] <- as.integer(names(g)[which.max(g)])
+    Tree.Struct[, 1] <- 1:nrow(Tree.Struct)
+    list(
+      Tree.Struct = Tree.Struct,
+      projbest.node = projbest.node,
+      splitCutoff.node = splitCutoff.node,
+      rep = rep,
+      rep1 = rep1,
+      rep2 = rep2
+    )
+    
+  } else {
+    Tree.Struct.row <- numeric(5)
+    Tree.Struct.row[1] <- id
+    Tree.Struct.row[2] <- rep1
+    rep1 <- rep1 + 1
+    Tree.Struct.row[3] <- rep1
+    rep1 <- rep1 + 1
+    Tree.Struct.row[4] <- rep2
+    rep2 <- rep2 + 1
+    a <- findproj_Ext(origclass, origdata, PPmethod, q = 1,  weight = TRUE, lambda)
+    Tree.Struct.row[5] <- a$Index
+    
+    Tree.Struct[id, ] <- Tree.Struct.row
+    
+    splitCutoff.node <- rbind(splitCutoff.node, a$C)
+    projbest.node <- rbind(projbest.node, matrix(a$Alpha, ncol = length(a$Alpha)))
+    t.class <- origclass
+    t.data <- origdata
+    t.class <- t.class * a$IOindexL
+    t.n <- length(t.class[t.class == 0])
+    t.index <- sort.list(t.class)
+    t.index <- sort(t.index[-(1:t.n)])
+    t.class <- t.class[t.index]
+    t.data <- origdata[t.index, ]
+    
+    cat("Node size:", length(origclass), "  tot:", tot, "  G:", G, "\n")
+    
+    b <- TreeExt.construct(origclass= t.class, origdata = t.data, Tree.Struct =Tree.Struct, id = Tree.Struct[id, 2],
+                           rep = rep, rep1=rep1, rep2=rep2, projbest.node =projbest.node, splitCutoff.node = splitCutoff.node,
+                           PPmethod = PPmethod,  lambda=lambda,srule=srule, tot=tot, tol=tol, ...)
+    
+    Tree.Struct <- b$Tree.Struct
+    projbest.node <- b$projbest.node
+    splitCutoff.node <- b$splitCutoff.node
+    rep <- b$rep
+    rep1 <- b$rep1
+    rep2 <- b$rep2
+    t.class <- origclass
+    t.data <- origdata
+    t.class <- (t.class * a$IOindexR)
+    t.n <- length(t.class[t.class == 0])
+    t.index <- sort.list(t.class)
+    t.index <- sort(t.index[-(1:t.n)])
+    t.class <- t.class[t.index]
+    t.data <- origdata[t.index, ]
+    n <- nrow(t.data)
+    G <- length(table(t.class))
+    
+    b <- TreeExt.construct(origclass=t.class, origdata=t.data, Tree.Struct=Tree.Struct, id =Tree.Struct[id, 3], rep=rep, rep1=rep1,
+                           rep2=rep2, projbest.node=projbest.node, splitCutoff.node=splitCutoff.node, PPmethod = PPmethod, 
+                           lambda =lambda,  srule=srule, tot=tot, tol=tol, ... )
+    Tree.Struct <- b$Tree.Struct
+    projbest.node <- b$projbest.node
+    splitCutoff.node <- b$splitCutoff.node
+    rep <- b$rep
+    rep1 <- b$rep1
+    rep2 <- b$rep2
+  }
+  list(Tree.Struct = Tree.Struct,
+    projbest.node = projbest.node,
+    splitCutoff.node = splitCutoff.node,
+    rep = rep,
+    rep1 = rep1,
+    rep2 = rep2
+  )
+}
