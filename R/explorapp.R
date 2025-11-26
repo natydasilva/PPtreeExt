3501f8df665ebd643c79832ae05f58a28d3cdb32
natydasilva <natalia.dasilva@fcea.edu.uy>
2025-11-26 23:55:25 +1100

    test error fix in app

--- R/explorapp.R
+++ R/explorapp.R
@@ -45,7 +45,7 @@ simu3 <-
 }
 
 
-ppbound <- function(ru, data , meth, entro , title, simM = FALSE) {
+ppbound <- function(ru, data , test, meth, entro , title, simM = FALSE) {
   grilla <-
     base::expand.grid(
     X1 = seq((min(data$X1) + sign(min(data$X1)) * .5), (max(data$X1) + sign(max(data$X1)) * .5), length.out = 100),
@@ -63,10 +63,10 @@ ppbound <- function(ru, data , meth, entro , title, simM = FALSE) {
       round(
         PPtreeViz::PPclassify(
           pptree,
-          test.data = data[, -1],
-          true.class = data[, 1],
+          test.data = test[, -1],
+          true.class = test[, 1],
           Rule = ru
-        )[[1]] / nrow(data[, -1]),
+        )[[1]] / nrow(test[, -1]),
         3
       ) * 100
   }
@@ -76,8 +76,8 @@ ppbound <- function(ru, data , meth, entro , title, simM = FALSE) {
       predict(rpart.mod, newdata = grilla, type = "class")
     err <-
       round(1 - sum(diag(table(
-        predict(rpart.mod, newdata = data[, -1], type = "class") , data[, 1]
-      ))) / nrow(data[, -1]), 3) * 100
+        predict(rpart.mod, newdata = test[, -1], type = "class") , test[, 1]
+      ))) / nrow(test[, -1]), 3) * 100
     
   }
   
@@ -97,10 +97,10 @@ ppbound <- function(ru, data , meth, entro , title, simM = FALSE) {
       round(
         PPtreeViz::PPclassify(
           pptree,
-          test.data = data[, -1],
-          true.class = data[, 1],
+          test.data = test[, -1],
+          true.class = test[, 1],
           Rule = ru
-        )[[1]] / nrow(data[, -1]),
+        )[[1]] / nrow(test[, -1]),
         3
       ) * 100
     
@@ -171,6 +171,7 @@ ppbound <- function(ru, data , meth, entro , title, simM = FALSE) {
 
 ppboundMOD <-
   function(data ,
+           test,
            meth = "MOD",
            entro = FALSE,
            entroindiv = TRUE,
@@ -202,8 +203,8 @@ ppboundMOD <-
     grilla$ppred <- ppred.sim[[2]]
     
     err <-
-      round(predict(object = pptree, newdata = data[, -1], true.class = data[, 1])[[1]] /
-              nrow(data[, -1]),
+      round(predict(object = pptree, newdata = test[, -1], true.class = test[, 1])[[1]] /
+              nrow(test[, -1]),
             3) * 100
     
     if (simM) {
@@ -294,18 +295,7 @@ ui <- shiny::fluidPage(shiny::mainPanel(
             choices = c("Subsetting clases" = "1", 
                          
                         "Multiple splits" = "3"),
-            selected = 1
-          )
-        ),
-        shiny::column(
-          3,
-          shiny::selectInput(
-            inputId = "stop",
-            label = "Stopping rule: Multiple splits",
-            choices = c("Pure node" = "1", 
-                                   "Node size" = "2",
-                                   "Entropy reduction" = "3"),
-            selected = 1
+            selected = 3
           )
         )
       ),
@@ -324,7 +314,7 @@ ui <- shiny::fluidPage(shiny::mainPanel(
           shiny::textInput(
             inputId = "cor",
             label = "Correlations",
-            value = "0.95, 0.95, 0.95"
+            value = "0.95, 0.5, 0.95"
           )
         ) ,
         shiny::column(
@@ -358,18 +348,7 @@ ui <- shiny::fluidPage(shiny::mainPanel(
             label = "Modification",
             choices = c("Subsetting clases" = "1", 
                          "Multiple splits" = "3"),
-            selected = 1
-          )
-        ),
-        shiny::column(
-          3,
-          shiny::selectInput(
-            inputId = "stop2",
-            label = "Stopping rule: Multiple splits",
-            choices = c("Pure node" = "1", 
-                        "Node size" = "2",
-                        "Entropy reduction" = "3"),
-            selected = 1
+            selected = 3
           )
         )
       ),
@@ -388,7 +367,7 @@ ui <- shiny::fluidPage(shiny::mainPanel(
           shiny::textInput(
             inputId = "cor2",
             label = "Correlations",
-            value = "0.95, 0.95, 0.95"
+            value = "0.95, 0.5, 0.75"
           )
         ) ,
         shiny::column(
@@ -406,7 +385,7 @@ ui <- shiny::fluidPage(shiny::mainPanel(
           inputId = "group",
           label = "Add outliers to class",
           choices = 1:3,
-          selected = 1
+          selected = 2
         )
       )),
       shiny::fluidRow(
@@ -432,7 +411,7 @@ ui <- shiny::fluidPage(shiny::mainPanel(
           shiny::textInput(
             inputId = "sampleout",
             label = "Out. sample size",
-            value = "10"
+            value = "50"
           )
         ),
         shiny::fluidRow(shiny::actionButton("do2", label = "OK"))
@@ -460,18 +439,7 @@ ui <- shiny::fluidPage(shiny::mainPanel(
             label = "Modification",
             choices = c("Subsetting clases" = "1", 
                        "Multiple splits" = "3"),
-            selected = 1
-          )
-        ),
-        shiny::column(
-          3,
-          shiny::selectInput(
-            inputId = "stop3",
-            label = "Stopping rule: Multiple splits",
-            choices = c("Pure node" = "1", 
-                        "Node size" = "2",
-                        "Entropy reduction" = "3"),
-            selected = 1
+            selected = 3
           )
         )
       ),
@@ -515,12 +483,16 @@ server <- function(input, output) {
       dat.pl2 <-
         shiny::isolate(simu3(x1[1], x1[2], x1[3], x1[4], x1[5], x1[6],
                              x2[1], x2[2], x2[3], x3[1], x3[2], x3[3]))
+      dat.test <-
+        shiny::isolate(simu3(x1[1], x1[2], x1[3], x1[4], x1[5], x1[6],
+                             x2[1], x2[2], x2[3], round(x3[1]*0.25), round(x3[2]*0.25), round(x3[3]*0.25)))
       
       if (input$modi == 1) {
         modpl <-
           ppbound(
             ru = 1,#as.numeric(input$rule),
             data = dat.pl2,
+            test = dat.test,
             meth = "Modified" ,
             entro = FALSE,
             title = "PPtreeExt: Subsetting clases"
@@ -532,6 +504,7 @@ server <- function(input, output) {
           ppbound(
             ru =  1, #as.numeric(input$rule),
             data = dat.pl2,
+            test = dat.test,
             meth = "Modified" ,
             entro = TRUE,
             title = "Modified 2 "
@@ -541,6 +514,7 @@ server <- function(input, output) {
         modpl <-
           ppboundMOD(
             data = dat.pl2,
+            test = dat.test,
             meth = "MOD",
             entro = FALSE,
             entroindiv = TRUE,
@@ -554,6 +528,7 @@ server <- function(input, output) {
         ppbound(
           ru =  as.numeric(input$rule),
           data = dat.pl2,
+          test = dat.test,
           meth = "Rpart",
           entro = TRUE ,
           title = "Rpart"
@@ -561,6 +536,7 @@ server <- function(input, output) {
         ppbound(
           ru =  as.numeric(input$rule),
           data = dat.pl2,
+          test =  dat.test,
           meth = "Original" ,
           entro = FALSE,
           title = "PPtree"
@@ -591,7 +567,9 @@ server <- function(input, output) {
         shiny::isolate(input$stop2)
       dat.pl2 <- simu3(x1[1], x1[2], x1[3], x1[4], x1[5], x1[6],
                        x2[1], x2[2], x2[3], x3[1], x3[2], x3[3])
-
+      dat.test <-
+        shiny::isolate(simu3(x1[1], x1[2], x1[3], x1[4], x1[5], x1[6],
+                             x2[1], x2[2], x2[3], round(x3[1]*0.25), round(x3[2]*0.25), round(x3[3]*0.25)))
 
       aux <-
         data.frame(
@@ -603,13 +581,26 @@ server <- function(input, output) {
                             mean = x4[2],
                             sd = x5[2])
         )
+      
+      aux2 <-
+        data.frame(
+          Sim = rep(paste("sim", as.numeric(input$group), sep = ""), round(x6*0.25)),
+          X1 = stats::rnorm(n = round(x6*0.25),
+                            mean = x4[1],
+                            sd = x5[1]),
+          X2 = stats::rnorm(n = round(x6*0.25),
+                            mean = x4[2],
+                            sd = x5[2])
+        )
       dat.pl2 <- rbind(dat.pl2, aux)
+      dat.test <- rbind(dat.test, aux2)
       
       if (input$modi2 == 1) {
         modpl <-
           ppbound(
             ru =  as.numeric(input$rule),
             data = dat.pl2,
+            test = dat.test,
             meth = "Modified" ,
             entro = FALSE,
             title = "PPtreeExt: Subsetting clases"
@@ -620,6 +611,7 @@ server <- function(input, output) {
           ppbound(
             ru =  as.numeric(input$rule),
             data = dat.pl2,
+            test = dat.test,
             meth = "Modified" ,
             entro = TRUE,
             title = "Modified 2"
@@ -629,6 +621,7 @@ server <- function(input, output) {
         modpl <-
           ppboundMOD(
             data = dat.pl2,
+            test = dat.test,
             meth = "MOD",
             entro = FALSE,
             entroindiv = TRUE,
@@ -642,6 +635,7 @@ server <- function(input, output) {
         ppbound(
           ru =  as.numeric(input$rule2),
           data = dat.pl2,
+          test = dat.test,
           meth = "Rpart" ,
           entro = FALSE,
           title = "Rpart"
@@ -649,6 +643,7 @@ server <- function(input, output) {
         ppbound(
           ru =  as.numeric(input$rule2),
           data = dat.pl2,
+          test = dat.test,
           meth = "Original",
           entro = TRUE,
           title = "PPtree"
@@ -695,19 +690,33 @@ server <- function(input, output) {
           Mu = Q$Mu,
           S = Q$S
         )
+      
+      Atest <-
+        MixSim::simdataset(
+          n = shiny::isolate(as.numeric(round(input$size*0.25))),
+          Pi = Q$Pi,
+          Mu = Q$Mu,
+          S = Q$S
+        )
       dat.pl2 <-
         data.frame(
           Sim = paste("sim", A[[2]], sep = ""),
           X1 = scale(A[[1]][, 1]),
           X2 = scale(A[[1]][, 2])
         )
-      
+      dat.test <-
+        data.frame(
+          Sim = paste("sim", Atest[[2]], sep = ""),
+          X1 = scale(Atest[[1]][, 1]),
+          X2 = scale(Atest[[1]][, 2])
+        )
       
       if (input$modi3 == 1) {
         modpl <-
           ppbound(
             ru =  as.numeric(input$rule3),
             data = dat.pl2,
+            test =dat.test,
             meth = "Modified" ,
             entro = FALSE,
             title = "PPtreeExt: Subsetting clases",
@@ -719,6 +728,7 @@ server <- function(input, output) {
           ppbound(
             ru =  as.numeric(input$rule3),
             data = dat.pl2,
+            test = dat.test,
             meth = "Modified" ,
             entro = TRUE,
             title = "Modified 2",
@@ -729,6 +739,7 @@ server <- function(input, output) {
         modpl <-
           ppboundMOD(
             data = dat.pl2,
+            test = dat.test,
             meth = "MOD",
             entro = FALSE,
             entroindiv = TRUE,
@@ -743,6 +754,7 @@ server <- function(input, output) {
         ppbound(
           ru = as.numeric(input$rule3),
           data = dat.pl2,
+          test = dat.test,
           meth = "Rpart",
           entro = TRUE ,
           title = "Rpart",
@@ -751,6 +763,7 @@ server <- function(input, output) {
         ppbound(
           ru = as.numeric(input$rule3),
           data = dat.pl2,
+          test = dat.test,
           meth = "Original" ,
           entro = FALSE,
           title = "PPtree",
